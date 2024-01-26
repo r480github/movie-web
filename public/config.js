6c2cbd50702956e813c35706114d110721cc6687
rogo! <119364101+r480github@users.noreply.github.com>
2024-01-26 18:45:22 -0500

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,9 +1,9 @@
 window.__CONFIG__ = {
   // The URL for the CORS proxy, the URL must NOT end with a slash!
-  VITE_CORS_PROXY_URL: "https://simple-proxy.09sharkboyroger.workers.dev"
+  VITE_CORS_PROXY_URL: "https://simple-proxy.09sharkboyroger.workers.dev",
 
   // The READ API key to access TMDB
-  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMmQ1MGJmNjFiOTU5ZDBiNmM5ZmRmZTAxYWU5NDFlOSIsInN1YiI6IjY0ZmQ1NTBjZmZjOWRlMGVkZWQyYzk2ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.hl8UAYO1Mt9DU8sKjv7x7QGU4oHs9j-V3_eD1nO43BM"
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMmQ1MGJmNjFiOTU5ZDBiNmM5ZmRmZTAxYWU5NDFlOSIsInN1YiI6IjY0ZmQ1NTBjZmZjOWRlMGVkZWQyYzk2ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.hl8UAYO1Mt9DU8sKjv7x7QGU4oHs9j-V3_eD1nO43BM",
 
   // The DMCA email displayed in the footer, null to hide the DMCA link
   VITE_DMCA_EMAIL: null,
